e0f3069ee562e68ae481afc76125996f6b051e7e
Mohammed Aman Nawaz <95523603+Nawaz-25@users.noreply.github.com>
2022-12-24 13:38:48 +0530

    first commit

---
+++ Portfolio_Website_React JS/src/contents/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import profilepic from '../img/naafi_photo.png';
+import Social from '../components/Social'
+
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="condiv home">
+            <img src={profilepic} alt="ProfilePic" className="profilepic"></img>
+            <ReactTypingEffect className="typingeffect" text={['I am Abdul Wahid Naafi','I am a web developer']} speed={100} eraseDelay={700}/>
+            <Social />
+            </div>
+            )
+        }
+    }
+    
+    export default Home
+    
